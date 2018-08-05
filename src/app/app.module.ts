1a5f18ba18a52879c6f4fe5806d8f84ed40ac1c0
Northerchild <osarus-13@hotmail.com>
2018-08-05 18:24:27 -0500

    Se crea la vista principal y los demas componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componets/shared/navbar/navbar.component';
 import { HomeComponent } from './componets/home/home.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { AboutComponent } from './componets/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
