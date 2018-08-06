866e861f22408e1f5af5e7500c875ba5b370ca45
Northerchild <osarus-13@hotmail.com>
2018-08-05 21:18:41 -0500

    Se agrega las demas rutas y se le dan una animacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componets/home/home.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { AboutComponent } from './componets/about/about.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', pathMatch: 'full', redirectTo:'home' },
 ];
 
